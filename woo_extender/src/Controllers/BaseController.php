23dd41e164bd37b80d4660b0eb2d199b5b31e555
Invisiblec <reza.sm2020@yahoo.com>
2026-09-10 19:02:04 +0330

    feat: enhance data handling in submission method to support edit action and prevent saving empty DTO

--- woo_extender/src/Controllers/BaseController.php
+++ woo_extender/src/Controllers/BaseController.php
@@ -145,14 +145,15 @@ abstract class BaseController
         $fields  = $this->service->getFormFields();
 
         try {
+            $dto = null;
 
             if ($_POST['form_action'] === 'edit') {
-                $factory_class::createChangeset($_POST, $fields);
+                $dto = $factory_class::createChangeset($_POST, $fields);
             } else {
                 $dto = $factory_class::createDTO($_POST, $fields);
             }
 
-            if (! $this->service->save($dto)) {
+            if (!empty($dto) && ! $this->service->save($dto)) {
                 throw new \Exception(__('Failed to save the data. Please try again.', 'woo-extender'));
             }
 
